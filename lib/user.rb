bf54bd6cbdb5703275c8b5e46a7c0b0ded45808e
Vicki Hamm <vicki.cello@gmail.com>
2018-06-28 21:13:39 +0000

    automatically backed up by learn

--- lib/user.rb
+++ lib/user.rb
@@ -2,8 +2,11 @@ class User
 
   attr_accessor :first_name, :last_name
 
-  def initialize(first_name, last_name)
+  def first_name
     @first_name = first_name
+  end
+
+  def last_name
     @last_name = last_name
   end
 
